741f072b36c86546fc1a191aa7573a1bc579af13
Kyle Hagler <khagler.kh@gmail.com>
2020-09-02 11:18:41 -0500

    Button now changes text

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [outputText, setOutputText] = useState('Open up App.js to start working on your app!');
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Button title="Say hello to mom" />
-      <Button title="Dont say hi" />
+      <Text>{outputText}</Text>
+      <Button title="Change Text" onPress={() => setOutputText('The text has changed')}/>
+      
     </View>
   );
 }
