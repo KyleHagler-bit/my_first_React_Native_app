795344fa5f28a05d65b4ced23c416424204a94e5
Kyle Hagler <khagler.kh@gmail.com>
2020-09-05 19:34:01 -0500

    Add more functionality

--- App.js
+++ App.js
@@ -1,27 +1,52 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  ScrollView,
+  FlatList,
+} from "react-native";
+//Use ScrollView when you know info will exceed page but not by much
+//Flatlist is better for "infinite" or unknown length of info
+
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
 
 export default function App() {
-  const [outputText, setOutputText] = useState(
-    "Open up App.js to start working on your app!"
-  );
+  
+  const [courseGoals, setCourseGoals] = useState([]);
+
+ 
+
+  const addGoalHandler = goalTitle => {
+    console.log(enteredGoal);
+    //setCourseGoals([...courseGoals, enteredGoal]);
+    setCourseGoals((currentGoals) => [
+      ...currentGoals,
+      { id: Math.random().toString(), value: goalTitle }, //need unique identifer
+    ]); //"gurantees latest state snapshot"
+  };
+
   return (
-    <View style={styles.container}>
-      <View style={{padding: 50}}>
-        <TextInput placeholder="Course Goal" style={{borderBottomColor: 'black', borderBottomWidth: 1 }} />
-        <Button title="ADD" />
-      </View>
-      <View></View>
+    <View style={styles.screen}>
+      <GoalInput onAddGoal={addGoalHandler}/>
+      {/*Need to use special view to allow moobile screen to scroll */}
+      <FlatList keyExtractor={(item, index) => item.id} data={courseGoals}>
+        renderItem=
+        {(itemData) => <GoalItem title={itemData.item.value}></GoalItem>}
+        {/*Wrap in view for more styling options */}
+      </FlatList>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+  screen: {
+    padding: 50,
   },
+  
+  
 });
